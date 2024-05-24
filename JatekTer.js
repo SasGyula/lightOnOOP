8f588ba634ac3f694edc46f44cbb280db6569dde
SasGyula <gyulasass43@gmail.com>
2024-05-24 11:20:43 +0200

    lighton alapok

---
+++ JatekTer.js
@@ -0,0 +1,11 @@
+export class Lampa{
+    
+    #allapot
+    #id
+    #divElem
+    constuctor(id, allapot, szuloElem){
+        this.#id = id
+        this.#allapot = allapot
+        this.#szuloElem = szuloElem
+    }
+}
---
+++ Lampa.js
@@ -0,0 +1,21 @@
+export class Lampa{
+    #db
+    #allapotLista
+    #meret
+    #lepes
+    constructor(){
+
+    }
+    #setAllapotLista(){
+
+    }
+    #szomszedokKeresese(id){
+
+    }
+    #init(){
+
+    }
+    #ellenorzes(){
+        
+    }
+}
