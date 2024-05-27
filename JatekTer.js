109e08ebc171e1c7b16fc9ed0addd140a715f33a
SasGyula <gyulasass43@gmail.com>
2024-05-27 19:24:20 +0200

    megjelenites

--- JatekTer.js
+++ JatekTer.js
@@ -1,11 +1,40 @@
-export class Lampa{
-    
-    #allapot
-    #id
-    #divElem
-    constuctor(id, allapot, szuloElem){
-        this.#id = id
-        this.#allapot = allapot
-        this.#szuloElem = szuloElem
+import { Lampa } from "./Lampa.js"
+export class JatekTer{
+    #db
+    #allapotLista
+    #meret
+    #lepes
+    constructor(){
+        this.#allapotLista = []
+        this.#meret = 3
+        this.#setAllapotLista()
+        this.#init()
     }
+    #setAllapotLista(){
+        for(let i = 0; i<this.#meret*this.#meret; i++){
+            if(Math.random < .2){
+                this.#allapotLista[i] = true
+            }else{
+                this.#allapotLista[i] = false
+            }
+        }
+    }
+    #szomszedokKeresese(id){
+
+    }
+    #init(){
+        const jatekter = $("#jatekter")
+        jatekter.empty();
+        for(let i = 0; i<this.#allapotLista.length; i++ ){
+            let elem = new Lampa(i, this.#allapotLista[i], jatekter)
+            
+        }
+    }
+    #ellenorzes(){
+        for(let i = 0; i<this.#allapotLista.length; i++){
+            if(this.#allapotLista[i] === true){
+                this.#db++
+            }
+        }
+       }
 }
--- Lampa.js
+++ Lampa.js
@@ -1,21 +1,36 @@
-export class Lampa{
-    #db
-    #allapotLista
-    #meret
-    #lepes
-    constructor(){
 
+export class Lampa{
+    #allapot
+    #id
+    #divElem
+    constuctor(id, allapot, divElem){
+        this.#id = id
+        this.#allapot = allapot
+        this.#divElem = divElem
+
+        this.#megjelenit();
+        
     }
-    #setAllapotLista(){
+    #megjelenit(){
+        let txt = ""
+        if(this.#allapot === true){
+            txt += `<div id="${this.#id}", class=""gombok0></div>`
+        }else if(this.#allapot === false){
+            txt += `<div id="${this.#id}", class=""gombok1></div>`
+        }
+        this.#divElem.append(txt)
+    }
+    #setAllapot(){
 
     }
     #szomszedokKeresese(id){
 
     }
-    #init(){
-
-    }
-    #ellenorzes(){
+    #szinBeallit(){
         
     }
+    #kattintasTrigger(){
+        
+    }
+
 }
